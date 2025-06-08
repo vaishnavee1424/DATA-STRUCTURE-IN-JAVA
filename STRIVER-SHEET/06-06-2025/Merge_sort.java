a3ce0f6d3aaa3d62fc5b5bbba685b59ef2ccd94a
Vaishnavee Prajapati <131327874+vaishnavee1424@users.noreply.github.com>
2025-06-08 20:56:27 +0530

    commit

--- STRIVER-SHEET/06-06-2025/Merge_sort.java
+++ STRIVER-SHEET/06-06-2025/Merge_sort.java
@@ -1,34 +1,57 @@
-// Let's look at the working of above example: 
 
-// Divide: 
-// [38, 27, 43, 10]  is divided into  [38, 27] and  [43, 10]  . 
-// [38, 27]  is divided into  [38]  and  [27]  . 
-// [43, 10]  is divided into  [43]  and  [10]  .
-
-// Conquer: 
-// [38]  is already sorted. 
-// [27]  is already sorted. 
-// [43]  is already sorted. 
-// [10]  is already sorted. 
-
-// Merge: 
-// Merge  [38]  and  [27]  to get  [27, 38]  . 
-// Merge  [43]  and  [10]  to get  [10,43]  . 
-// Merge  [27, 38]  and  [10,43]  to get the final sorted list  [10, 27, 38, 43] 
-// Therefore, the sorted list is  [10, 27, 38, 43]. 
-
-// Approach : 
-// Divide the array into two parts until only single remain 
-// then compare each of them 
-// consequently compare and merge it until the length is equal to 
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Merge_sort {
     
     public static void main(String[] args){
-    
-    }
+     int [] arr={1,2,4,1,2};
+     int low=0;
+     int high =arr.length;
+     merge_sort(arr, low, high);
 
-    static void swap(){
-    
-    }
+     System.out.println(Arrays.toString(arr));
 
+    }
+    public static void merge_sort(int []arr,int low ,int high){
+   if(low<=high)return;
+       
+      int mid= (low+high)/2;
+   
+      merge_sort( arr, low , mid);
+      merge_sort(arr, mid+1,high);
+      merge(arr,low,mid,high);
+    }
+    public static void merge(int [] arr, int  low, int  mid ,int high){
+      ArrayList <Integer> temp = new ArrayList<>();
+      // there will be a left and right pointer from comapring low to mid and mid+1 to high;
+      int left=low;
+      int right=mid+1;
+      while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.add(arr[left]);
+            left++;
+        }
+        else {
+            temp.add(arr[right]);
+            right++;
+        }
+
+      }
+      while(left<=mid){
+        temp.add(arr[left]);
+        left++;
+      }
+      while(right<=high){
+        temp.add(arr[right]);
+        right++;
+      }
+      // for loop arr mai temp ke elements dalane ke liye;
+      for(int i=low ;i<high;i++){
+        // arr[i]=temp[i-low];
+
+         arr[i]=temp.get(i-low);
+        
+      }
+    }
+   
 }
---
+++ STRIVER-SHEET/06-06-2025/N_meeting.java
@@ -0,0 +1,7 @@
+// package STRIVER-SHEET.06-06-2025;
+
+// public class N_meeting {
+    
+// }
+
+// understand the question but not the code , did cheating 
